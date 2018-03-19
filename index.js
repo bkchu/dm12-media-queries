8c51856abbe3b1518afbaad475c1d4b36cb671d0
bkchu <brandon.k.chung@gmail.com>
2018-03-19 18:00:07 -0500

    took out console.log

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const menuButton = document.querySelector(".nav__menu");
 const dropdown = document.querySelector(".dropdown");
 
 function clicked() {
-  console.log("clicked");
   dropdown.classList.toggle("dropdown--show");
 }
